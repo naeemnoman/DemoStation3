4aa7b22223efe1256681cd06b0ee7dbb26360504
naeemnoman <naeemnoman0419@gmail.com>
2024-11-09 17:05:39 +0600

    done

---
+++ app/src/main/java/com/example/job3loactionshareapp/ProfileFragment.kt
@@ -0,0 +1,90 @@
+package com.example.job3loactionshareapp
+
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.job3loactionshareapp.databinding.FragmentProfileBinding
+import com.example.job3loactionshareapp.view.LoginActivity
+import com.example.job3loactionshareapp.view.MainActivity
+import com.example.job3loactionshareapp.viewmodel.AuthenticationViewmodel
+import com.example.job3loactionshareapp.viewmodel.FireStoreViewmodel
+import com.example.job3loactionshareapp.viewmodel.LocationViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var authViewModel: AuthenticationViewmodel
+    private lateinit var firestoreViewModel: FireStoreViewmodel
+    private lateinit var locationViewModel: LocationViewModel
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        authViewModel = ViewModelProvider(this).get(AuthenticationViewmodel::class.java)
+        firestoreViewModel = ViewModelProvider(this).get(FireStoreViewmodel::class.java)
+        locationViewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
+
+
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+
+        }
+
+        binding.homeBtn.setOnClickListener {
+            startActivity(Intent(this.context, MainActivity::class.java))
+        }
+
+        loadUserInfo()
+        binding.updateBtn.setOnClickListener {
+            val newName = binding.NameEt.text.toString()
+            val newLocation = binding.Loaction.text.toString()
+
+            updateBtn(newName, newLocation)
+        }
+
+        return binding.root
+    }
+
+    private fun updateBtn(newName: String, newLocation: String) {
+        val currentUser = authViewModel.getCurrentUser()
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            firestoreViewModel.updateUser(requireContext(), userId, newName, newLocation)
+            Toast.makeText(requireContext(), "Profile updated successfully", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(requireContext(), MainActivity::class.java))
+        } else {
+            Toast.makeText(requireContext(), "User not found", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loadUserInfo() {
+        val currentUser = authViewModel.getCurrentUser()
+        if(currentUser != null) {
+            binding.emailEt.setText(currentUser.email)
+            firestoreViewModel.getUser(requireContext(), currentUser.uid){ user ->
+                if (currentUser.displayName != null) {
+                    binding.NameEt.setText(currentUser.displayName)
+                }
+            }
+        }else {
+            Toast.makeText(requireContext(), "User not found", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
